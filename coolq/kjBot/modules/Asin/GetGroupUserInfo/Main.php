74f19098f8e0521fa310e9db7577c270a9b33f1b
子不语 <1063614727@qq.com>
2019-03-01 17:27:29 +0800

    测试代码

--- coolq/kjBot/modules/Asin/GetGroupUserInfo/Main.php
+++ coolq/kjBot/modules/Asin/GetGroupUserInfo/Main.php
@@ -9,6 +9,7 @@ class Main extends Module
 {
 
     public function process(array $args, MessageEvent $event){
+    	Log::Debug('-->',json_encode($this));
     	return $event->sendBack(json_encode($this->senderInfo));
         // return $event->sendBack('Hello, world!');
     }

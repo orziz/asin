9cc8a7eb8186788ca1fb3fe07ffda513a28c6af1
子不语 <1063614727@qq.com>
2019-03-01 19:57:59 +0800

    测试代码

---
+++ api.php
@@ -0,0 +1,48 @@
+<?php
+
+
+/**
+ * 此文件为 api 唯一入口文件
+ */
+
+// 加载核心库
+require_once 'source/class/class_core.php';
+
+// 预设置返回内容
+$res = array();
+
+// 获取响应内容
+$param = getgpc('param');
+
+if (empty($param)) {
+	// 如果没有响应内容，则返回错误信息
+	$res['errCode'] = -1;
+	$res['errMsg'] = '没有参数';
+} else {
+
+	// 获取响应模组
+	$mod = getgpc('mod','PARAM');
+
+	if (empty($mod) || (false === strpos($mod, '_'))) {
+		// 如果没有响应模组或者请求模组中未携带_，则返回错误信息
+		$res['errCode'] = -2;
+		$res['errMsg'] = '没有请求模组';
+	} else {
+		$modArr = explode('_',$mod);
+		$mod = implode('/',$modArr);
+		$modPath = libfile($mod,'module');
+		if (file_exists($modPath)) {
+			// 如果有该文件则加载
+			// 获取响应行动
+			$action = getgpc('action','PARAM');
+			require_once $modPath;
+		} else {
+			// 如果没有该文件，则返回错误信息
+			$res['errCode'] = -3;
+			$res['errMsg'] = '没有该模组';
+		}
+	}
+}
+
+// 返回信息
+echo json_encode($res);

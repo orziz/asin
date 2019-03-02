de2b9dd6238195d62ec1a2de784d062e9acf460d
子不语 <1063614727@qq.com>
2019-03-02 21:05:25 +0800

    新增获取发送者信息

--- coolq/kjBot/modules/Asin/Home/SenderInfo.php
+++ coolq/kjBot/modules/Asin/Home/SenderInfo.php
@@ -24,7 +24,24 @@ class SenderInfo extends Module
 		$senderInfo = $event->getSenderInfo();
 		$msg .= 'QQ：'.$User_id;
 		foreach ($senderInfo as $key => $value) {
-			$msg .= "\n".$key.'：'.$value;
+			if (!$value || $key == 'isGroupSender') continue;
+			// nickname：欧阳尘星
+			// isGroupSender：1
+			// card：江苏-欧阳尘星
+			// level：刺客大师
+			// role：admin
+			// title：人事部经理
+			if ($key == 'nickname'）$k = '昵称';
+			if ($key == 'card'）$k = '群昵称';
+			if ($key == 'level'）$k = '聊天等级';
+			if ($key == 'title'）$k = '头衔';
+			if ($key == 'role') {
+				$k = '权限';
+				if ($value == 'owner') $value = '群主';
+				if ($value == 'admin') $value = '管理员';
+				if ($value == 'member') $value = '成员';
+			}
+			$msg .= "\n".$k.'：'.$value;
 		}
 		return $event->sendBack($msg);
 	}

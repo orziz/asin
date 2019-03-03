3b23b1f6749ee2ac31e7693ffb587a06a8c24b6a
子不语 <1063614727@qq.com>
2019-03-03 18:41:58 +0800

    提交签到测试版本

--- coolq/kjBot/modules.php
+++ coolq/kjBot/modules.php
@@ -1,8 +1,9 @@
 <?php
 
 $Modules = [
-    '你好' => kjBotModule\Hello\Main::class,
-    '获取群消息' => kjBotModule\Asin\GetGroupUserInfo\Main::class,
-    '信息' => kjBotModule\Asin\Home\Info::class,
-    '获取我的信息' => kjBotModule\Asin\Home\SenderInfo::class
+	'你好' => kjBotModule\Hello\Main::class,
+	'获取群消息' => kjBotModule\Asin\GetGroupUserInfo\Main::class,
+	'信息' => kjBotModule\Asin\Home\Info::class,
+	'获取我的信息' => kjBotModule\Asin\Home\SenderInfo::class,
+	'签到' => kjBotModule\Asin\Home\CheckIn::class
 ];

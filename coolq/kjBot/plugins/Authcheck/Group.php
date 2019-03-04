d5ab615f89766452b38927cb66c4685f1256464d
子不语 <1063614727@qq.com>
2019-03-04 13:37:25 +0800

    提交加入刺客组织代码

--- coolq/kjBot/plugins/Authcheck/Group.php
+++ coolq/kjBot/plugins/Authcheck/Group.php
@@ -20,11 +20,11 @@ class Group extends Plugin {
                 if ((false !== strpos($event->getMsg(), '怎么') || false !== strpos($event->getMsg(), '如何')) && false !== strpos($event->getMsg(), '加入')) {
                     return $event->sendBack(CQCode::At($event->getId()).' 暂不支持自动加入刺客组织，请联系千刃');
                 } 
+                return $event->sendBack(json_encode($modules));
                 array_push($modules, array(
                     '加入刺客组织' => kjBotModule\Asin\Home\JoinOrgan::class
                 ));
             }
-            $msg .= CQCode::At($User_id).' ';
         }
         return NULL;
     }

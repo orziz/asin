53a3f00bdcd1900d7847ee6337ce03b3eb16caa5
子不语 <1063614727@qq.com>
2019-03-06 01:03:38 +0800

    新增获取信息

--- coolq/kjBot/modules/Asin/GroupControl/KickDead.php
+++ coolq/kjBot/modules/Asin/GroupControl/KickDead.php
@@ -15,7 +15,8 @@ class KickDead extends Module
         if ($User_id != '1063614727') q('权限不足');
         global $kjBot;
     	$userList = $kjBot->getCoolQ()->getGroupMemberList($event->groupId);
-        for ($i=0; $i < count($userList); $i++) { 
+        Log::Debug('userList=>'.json_encode($userList));
+        for ($i=0; $i < count($userList); $i++) {
             $memberInfo = $userList[$i];
             if ($memberInfo->last_sent_time < 1533052801) {
                 Log::Debug('清除死鱼====>'.$memberInfo->user_id);

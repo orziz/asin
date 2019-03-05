c14391798bbb943ac9f6ead1238ae9c99fb42b4e
子不语 <1063614727@qq.com>
2019-03-05 20:27:34 +0800

    新增签到赞助信息

--- coolq/kjBot/modules/Asin/Home/Checkin.php
+++ coolq/kjBot/modules/Asin/Home/Checkin.php
@@ -19,9 +19,11 @@ class Checkin extends Module
 			$msg .= CQCode::At($User_id).' ';
 		}
 		$data = param_post('http://asin.ygame.cc/api.php',array('mod'=>'home_checkin','action'=>'checkin','qq'=>$User_id));
-		if ($data['errCode'] === 301) return $event->sendBack($msg.'签到失败：暂没有加入刺客组织');
-		if ($data['errCode'] === 302) return $event->sendBack($msg.'签到失败：今天您已签到，无需重复签到');
-		if ($data['errCode'] === 303) return $event->sendBack($msg.'签到失败');
-		return $event->sendBack($msg."恭喜你签到成功啦，可惜没奖励……\n你已连续签到 ".$data['data']['count'].' 天，请继续保持');
+		if ($data['errCode'] === 301) $msg .= '签到失败：暂没有加入刺客组织';
+		elseif ($data['errCode'] === 302) $msg .= '签到失败：今天您已签到，无需重复签到';
+		elseif ($data['errCode'] === 303) $msg .= '签到失败';
+		else $msg .= "恭喜你签到成功啦，可惜没奖励……\n你已连续签到 ".$data['data']['count'].' 天，请继续保持';
+		$msg .= "\n该功能由 ".CQCode::At(1845896706).' 赞助';
+		return $event->sendBack($msg);
 	}
 }

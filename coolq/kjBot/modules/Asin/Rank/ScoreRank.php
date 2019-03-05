0a70a895a303df419e1370f4254e3def34c6b68b
子不语 <1063614727@qq.com>
2019-03-05 22:31:40 +0800

    新增签到赞助信息

--- coolq/kjBot/modules/Asin/Rank/ScoreRank.php
+++ coolq/kjBot/modules/Asin/Rank/ScoreRank.php
@@ -17,7 +17,7 @@ class ScoreRank extends Module
 		$data = param_post('http://asin.ygame.cc/api.php',array('mod'=>'rank_score','action'=>'getRankList','num'=>10));
 		$rankList = $data['data'];
 		for ($i=0; $i < count($rankList); $i++) {
-			$msg .= "\n".$rankList[$i]['rank']."\t".$rankList[$i]['nickname']."\t".$rankList[$i]['score'];
+			$msg .= "\n".$rankList[$i]['rank']."\t\t".$rankList[$i]['nickname']."\t".$rankList[$i]['score'];
 		}
 		return $event->sendBack($msg);
 	}

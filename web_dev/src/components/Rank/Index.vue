6bd94d887bc88e4ba94fa8755a7b82f167392946
子不语 <1063614727@qq.com>
2019-03-05 15:58:16 +0800

    新增网页版排行榜

--- web_dev/src/components/Rank/Index.vue
+++ web_dev/src/components/Rank/Index.vue
@@ -1,6 +1,17 @@
 <template>
 	<div id="main">
-		<h2>{{ msg }}</h2>
+		<ul>
+			<li class="top">
+				<span class="rank">排名</span>
+				<span class="nickname">姓名</span>
+				<span class="score">积分</span>
+			</li>
+			<li v-for="item in rankList">
+				<span class="rank">{{ item.rank }}</span>
+				<span class="nickname">{{ item.nickname }}</span>
+				<span class="score">{{ item.score }}</span>
+			</li>
+		</ul>
 	</div>
 </template>
 
@@ -16,8 +27,12 @@ export default {
 	mounted: function() {
 		console.log(this);
 		orzzz.$post({
-			mod: 'rank_rank',
-			action: 'getRankList'
+			mod: 'rank_score',
+			action: 'getRankList',
+			success: (res)=>{
+				console.log(res);
+				this.rankList = res
+			}
 		})
 	}
 };
@@ -25,16 +40,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ul { list-style-type: none; padding: 0; margin: 10px auto; }
+li { display: block; font-size: 2em; border-bottom: 1px solid #dedede; }
+li.top { font-size: 4em; font-weight: bold; width: 100%; border-top: 1px solid #dedede; }
+span { display: inline-block; }
+span.rank { width: 30%; }
+span.nickname { width: 35%; }
+span.score { width: 30%; }
 h1, h2 {
   font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;

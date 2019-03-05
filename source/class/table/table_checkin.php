aff2437fc3b1398b9375bee2ee053822cba38290
子不语 <1063614727@qq.com>
2019-03-05 11:59:17 +0800

    提交加入刺客组织代码

--- source/class/table/table_checkin.php
+++ source/class/table/table_checkin.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * 
+ * 签到相关数据库操作
  */
 class table_checkin extends Table
 {
@@ -14,12 +14,25 @@ class table_checkin extends Table
 		parent::__construct();
 	}
 
+	/**
+	 * 根据日期获取签到信息
+	 * @param  [type] $day 需要获取签到信息的日期
+	 * @param  [type] $db  [description]
+	 * @return [type]      [description]
+	 */
 	public function getCheckinByDay($day=null,$db=null) {
 		if (!$db) global $db;
 		if (!$day) $day = getTime('Y-m-d');
 		return $db->fetch($this->_table,array('lday'=>$day));
 	}
 
+	/**
+	 * 新建用户签到信息
+	 * @param  [type] $pk    [description]
+	 * @param  array  $datas [description]
+	 * @param  [type] $db    [description]
+	 * @return [type]        [description]
+	 */
 	protected function newData($pk,array $datas,$db) {
 		$day = getTime('Y-m-d');
 		$datas['count'] = 1;
@@ -27,6 +40,13 @@ class table_checkin extends Table
 		return parent::newData($pk,$datas,$db);
 	}
 
+	/**
+	 * 更新用户签到信息
+	 * @param  [type] $pk    [description]
+	 * @param  array  $datas [description]
+	 * @param  [type] $db    [description]
+	 * @return [type]        [description]
+	 */
 	protected function updateData($pk,array $datas,$db) {
 		$data = $this->getData($qq,$db);
 		$yday = getTime('Y-m-d',time()-86400);
@@ -38,13 +58,18 @@ class table_checkin extends Table
 		return parent::updateData($pk,$datas,$db);
 	}
 
-	public function getCont($db,$qq) {
-		if (!$qq) return 0;
-		date_default_timezone_set('Asia/Shanghai');
-		$day = date('Y-m-d', time()-86400);
-		$data = $this->getCheckinByDay($db,$day);
-		if (!$data) return 0;
-		return ($data["$qq"] ? $data["$qq"]['cont'] : 0);
+	/**
+	 * 检测今天是否已签到
+	 * @param  [type]  $pk [description]
+	 * @param  [type]  $db [description]
+	 * @return boolean     [description]
+	 */
+	public function isCheckin($pk,$db=null): boolean {
+		if (!$db) global $db;
+		if (!$pk) return false;
+		$data = $this->getData($pk,$db);
+		if (!$data) return false;
+		return ($data['lday'] == getTime('Y-m-d'));
 	}
 
 }
--- source/module/home/home_checkin.php
+++ source/module/home/home_checkin.php
@@ -9,15 +9,21 @@ if ($action == 'checkin') {
 		$res['errCode'] = 301;
 		$res['errMsg'] = '没有该用户';
 	} else {
-		$isSuccess = C::t('checkin')->setData($qq);
-		if (!$isSuccess) {
+		$isCheckin = C::t('checkin')->isCheckin();
+		if ($isCheckin) {
 			$res['errCode'] = 302;
-			$res['errMsg'] = '签到失败';
+			$res['errMsg'] = '今天已签到';
 		} else {
-			$data = C::t('checkin')->getData($qq);
-			$res['errCode'] = 200;
-			$res['errMsg'] = '签到成功';
-			$res['data'] = $data;
+			$isSuccess = C::t('checkin')->setData($qq);
+			if (!$isSuccess) {
+				$res['errCode'] = 303;
+				$res['errMsg'] = '签到失败';
+			} else {
+				$data = C::t('checkin')->getData($qq);
+				$res['errCode'] = 200;
+				$res['errMsg'] = '签到成功';
+				$res['data'] = $data;
+			}
 		}
 	}
 }

51b80a5941ab07e8deb0079621fb1b2c31b8e7da
子不语 <1063614727@qq.com>
2019-03-06 18:26:12 +0800

    提交代码

--- coolq/kjBot/modules/Common/GroupManager/KickDead.php
+++ coolq/kjBot/modules/Common/GroupManager/KickDead.php
@@ -15,15 +15,15 @@ class KickDead extends Module
 
     const needCQ = true;
 
-    public function processWithCQ(array $args, $event, $CQ=null){
+    public function processWithCQ(array $args, $event,kjBot\SDK\CoolQ $cq){
         checkAuth($event);
         $User_id = $event->getId();
         $senderInfo = $event->getSenderInfo();
-    	$userList = $CQ->getGroupMemberList($event->groupId);
+    	$userList = $cq->getGroupMemberList($event->groupId);
         for ($i=0; $i < count($userList); $i++) {
             $memberInfo = $userList[$i];
             if ($memberInfo->last_sent_time < 1546272001) {
-                $CQ->setGroupKick($event->groupId,$memberInfo->user_id);
+                $cq->setGroupKick($event->groupId,$memberInfo->user_id);
             }
         }
     	return $event->sendBack('清除成功');

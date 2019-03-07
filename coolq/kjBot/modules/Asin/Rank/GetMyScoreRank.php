e9e24ed80c67fd8f27fdd2306d643d6229d80478
子不语 <1063614727@qq.com>
2019-03-07 18:28:57 +0800

    新增用户登录

---
+++ coolq/kjBot/modules/Asin/Rank/GetMyScoreRank.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace kjBotModule\Asin\Rank;
+
+use kjBot\Framework\Module;
+use kjBot\Framework\Event\GroupMessageEvent;
+use kjBot\SDK\CQCode;
+
+/**
+ * 
+ */
+class GetMyScoreRank extends Module
+{
+	
+	public function process(array $args, $event){
+		$msg = '';
+		$User_id = $event->getId();
+		if($event instanceof GroupMessageEvent) $msg .= CQCode::At($User_id).' ';
+		$atqq = isset($args[1]) ? parseQQ($args[1]) : null;
+		$qq = $atqq ? $atqq : $User_id;
+		$data = param_post('http://asin.ygame.cc/api.php',array('mod'=>'rank_score','action'=>'getMyRank','qq'=>$qq));
+		if ($data['errCode'] === 200) {
+			if ($qq == $User_id) $msg .= '您当前的排名为： '.$data['data']['rank'].' ，请继续努力提高排名';
+			else $msg .= '您查询的用户当前排名为：'.$data['data']['rank'];
+		} elseif ($data['errCode'] === 301) {
+			if ($qq == $User_id) $msg .= '您暂时没有加入刺客组织';
+			else $msg .= '您查询的用户暂时没有加入刺客组织';
+		} else {
+			$msg .= '查询失败';
+		}
+		$msg .= "\n查看完整排行榜请点击链接 http://asin.ygame.cc/rank";
+		return $event->sendBack($msg);
+	}
+}

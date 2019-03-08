483376c5245bf2b187afac5af0cc5eb4554c3391
子不语 <1063614727@qq.com>
2019-03-08 14:47:26 +0800

    测试socket

--- coolq/kjBot/public/socket.php
+++ coolq/kjBot/public/socket.php
@@ -33,16 +33,19 @@ if ($ref < 0) {
 	echo '链接失败';
 }
 do {
-    if (($msgsock = socket_accept($sock)) < 0) {
-        echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";
-    	Log::Debug("socket_accept() failed: reason: " . socket_strerror($msgsock));
-    } else {
-        //发到客户端
-        $msg ="测试成功！\n";
-        // socket_write($msgsock, $msg, strlen($msg));
-        $buf = socket_read($msgsock,8192);
-        $talkback = "收到的信息:$buf\n";
-        echo $talkback;
+    // if (($msgsock = socket_accept($sock)) < 0) {
+    //     echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";
+    // 	Log::Debug("socket_accept() failed: reason: " . socket_strerror($msgsock));
+    // } else {
+    //     //发到客户端
+    //     $msg ="测试成功！\n";
+    //     // socket_write($msgsock, $msg, strlen($msg));
+    //     $buf = socket_read($msgsock,8192);
+    //     $talkback = "收到的信息:$buf\n";
+    //     echo $talkback;
+    // }
+    if ($msg = socket_read($sock,8192)) {
+    	echo "收到的信息:$msg\n";
     }
     //echo $buf;
     // socket_close($msgsock);

8d4aa50adaa361a92cb27aaf241339dfe9c8c4aa
子不语 <1063614727@qq.com>
2019-03-08 14:38:16 +0800

    测试socket

--- coolq/kjBot/public/socket.php
+++ coolq/kjBot/public/socket.php
@@ -2,7 +2,7 @@
 
 require_once '../../../source/class/class_core.php';
 set_time_limit(0); //限制执行时间  0为不限制
-$ip = '0.0.0.0';
+$ip = '127.0.0.1';
 $port = 7001;//端口
 /**
 socket通信整个过程 
@@ -15,20 +15,23 @@ socket_write //将数据写到 socket 缓存 向客户端发送
 socket_close   //关闭套接字资源
 */
 
-system('echo 11111');
+echo '启动socket';
 if(($sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP)) < 0) {
     echo "socket_create() 失败的原因是:".socket_strerror($sock)."\n";
     Log::Debug("socket_create() 失败的原因是:".socket_strerror($sock));
 }
-if(($ret = socket_bind($sock,$ip,$port)) < 0) {
-    echo "socket_bind() 失败的原因是:".socket_strerror($ret)."\n";
-    Log::Debug("socket_bind() 失败的原因是:".socket_strerror($ret));
+// if(($ret = socket_bind($sock,$ip,$port)) < 0) {
+//     echo "socket_bind() 失败的原因是:".socket_strerror($ret)."\n";
+//     Log::Debug("socket_bind() 失败的原因是:".socket_strerror($ret));
+// }
+// if(($ret = socket_listen($sock,4)) < 0) {
+//     echo "socket_listen() 失败的原因是:".socket_strerror($ret)."\n";
+//     Log::Debug("socket_listen() 失败的原因是:".socket_strerror($ret));
+// }
+$ref = socket_connect($sock, $ip, $port);
+if ($ref < 0) {
+	echo '链接失败';
 }
-if(($ret = socket_listen($sock,4)) < 0) {
-    echo "socket_listen() 失败的原因是:".socket_strerror($ret)."\n";
-    Log::Debug("socket_listen() 失败的原因是:".socket_strerror($ret));
-}
-$count = 0;
 do {
     if (($msgsock = socket_accept($sock)) < 0) {
         echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";

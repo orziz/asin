243d41eddc8e46c031945219db4b2163ec38ecbe
子不语 <1063614727@qq.com>
2019-03-08 15:27:56 +0800

    测试socket

--- coolq/kjBot/public/socket.php
+++ coolq/kjBot/public/socket.php
@@ -19,43 +19,43 @@ echo '启动socket';
 if(($sock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP)) < 0) {
     echo "socket_create() 失败的原因是:".socket_strerror($sock)."\n";
 }
-// if(($ret = socket_bind($sock,$ip,$port)) < 0) {
-//     echo "socket_bind() 失败的原因是:".socket_strerror($ret)."\n";
-//     Log::Debug("socket_bind() 失败的原因是:".socket_strerror($ret));
-// }
-// if(($ret = socket_listen($sock,4)) < 0) {
-//     echo "socket_listen() 失败的原因是:".socket_strerror($ret)."\n";
-//     Log::Debug("socket_listen() 失败的原因是:".socket_strerror($ret));
-// }
-$ref = socket_connect($sock, $ip, $port);
-if ($ref < 0) {
-	echo '连接失败';
-} else {
-	echo '连接成功';
+if(($ret = socket_bind($sock,$ip,$port)) < 0) {
+    echo "socket_bind() 失败的原因是:".socket_strerror($ret)."\n";
+    Log::Debug("socket_bind() 失败的原因是:".socket_strerror($ret));
 }
-while ($msg = socket_read($sock,8192)) {
-	echo '收到消息';
-	usleep(100);
+if(($ret = socket_listen($sock,4)) < 0) {
+    echo "socket_listen() 失败的原因是:".socket_strerror($ret)."\n";
+    Log::Debug("socket_listen() 失败的原因是:".socket_strerror($ret));
 }
-// do {
-//     // if (($msgsock = socket_accept($sock)) < 0) {
-//     //     echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";
-//     // 	Log::Debug("socket_accept() failed: reason: " . socket_strerror($msgsock));
-//     // } else {
-//     //     //发到客户端
-//     //     $msg ="测试成功！\n";
-//     //     // socket_write($msgsock, $msg, strlen($msg));
-//     //     $buf = socket_read($msgsock,8192);
-//     //     $talkback = "收到的信息:$buf\n";
-//     //     echo $talkback;
-//     // }
-//     if ($msg = socket_read($sock,8192)) {
-//     	echo '收到消息';
-//     	// echo "收到的信息:$msg\n";
-//     }
-//     //echo $buf;
-//     // socket_close($msgsock);
-//     // 
-//     usleep(5000);
-// } while (true);
+// $ref = socket_connect($sock, $ip, $port);
+// if ($ref < 0) {
+// 	echo '连接失败';
+// } else {
+// 	echo '连接成功';
+// }
+// while ($msg = socket_read($sock,8192)) {
+// 	echo '收到消息';
+// 	usleep(100);
+// }
+do {
+    if (($msgsock = socket_accept($sock)) < 0) {
+        echo "socket_accept() failed: reason: " . socket_strerror($msgsock) . "\n";
+    	Log::Debug("socket_accept() failed: reason: " . socket_strerror($msgsock));
+    } else {
+        //发到客户端
+        $msg ="测试成功！\n";
+        // socket_write($msgsock, $msg, strlen($msg));
+        $buf = socket_read($msgsock,8192);
+        $talkback = "收到的信息:$buf\n";
+        echo $talkback;
+    }
+    // if ($msg = socket_read($sock,8192)) {
+    // 	echo '收到消息';
+    // 	// echo "收到的信息:$msg\n";
+    // }
+    //echo $buf;
+    // socket_close($msgsock);
+    // 
+    usleep(5000);
+} while (true);
 // socket_close($sock);

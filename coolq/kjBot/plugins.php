521c48d677494c6d72c3ebaffe22778e28d8374d
子不语 <1063614727@qq.com>
2019-03-15 23:04:59 +0800

    提交代码

--- coolq/kjBot/plugins.php
+++ coolq/kjBot/plugins.php
@@ -3,5 +3,6 @@
 $Plugins = [
     kjBotPlugin\GroupCheck\Common\_Message::class,
     kjBotPlugin\GroupCheck\AsinGroup\_Message::class,
-    kjBotPlugin\GroupCheck\AsinGroup\_Notice::class
+    kjBotPlugin\GroupCheck\AsinGroup\_Notice::class,
+    kjBotPlugin\GroupCheck\AsinGroup\_Meta_event::class
 ];
---
+++ coolq/kjBot/plugins/GroupCheck/AsinGroup/_Meta_event.php
@@ -0,0 +1,25 @@
+<?php
+namespace kjBotPlugin\GroupCheck\AsinGroup;
+
+use kjBot\Framework\Plugin;
+use kjBot\Framework\Message;
+use kjBot\Framework\Event\GroupMessageEvent;
+use kjBot\SDK\CQCode;
+use \Log;
+
+class _Meta_event extends Plugin {
+    public $handleDepth = 2; //捕获到最底层的事件
+    public $handleQueue = true; //声明是否要捕获消息队列
+
+    public function beforePostMessage(&$queue){} //若声明不需要捕获消息队列可不实现本方法
+    //此处以正常群聊消息举例
+    public function meta_event_heartbeat($event) {
+        $Queue[] = $this->test($event);
+        return $Queue;
+    }
+
+    private function test($event) {
+        return $event->sendTo(TargetType::Group,'719994813','心跳测试：：：'.time());
+    }
+
+}

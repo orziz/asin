9b0e7336ecb8d728b65486be0ca2c981e5b5e595
子不语 <1063614727@qq.com>
2019-03-15 22:36:31 +0800

    提交代码

--- coolq/kjBot/plugins.php
+++ coolq/kjBot/plugins.php
@@ -1,6 +1,7 @@
 <?php
 
 $Plugins = [
+    kjBotPlugin\GroupCheck\Common\_Message::class,
     kjBotPlugin\GroupCheck\AsinGroup\_Message::class,
     kjBotPlugin\GroupCheck\AsinGroup\_Notice::class
 ];

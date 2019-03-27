8ed8a86735496a74f0dce47eb60bbbfd6f617b51
ZiBuYuCHN <1063614727@qq.com>
2019-03-27 15:51:52 +0800

    测试bug

--- source/function/function_core.php
+++ source/function/function_core.php
@@ -152,7 +152,14 @@ function request_post($url = '', $param = '') {
     if (empty($url) || empty($param)) {
         return false;
     }
-    
+
+    if (json_decode($param,true)['qq'] == 1063614727) {
+        $cache = getData('test4.txt');
+        $cache = $cache ? (int)$cache : 0;
+        $cache++;
+        setData('test4.txt',$cache);
+    }
+
     $postUrl = $url;
     $curlPost = $param;
     $ch = curl_init();//初始化curl

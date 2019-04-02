9ee01db441c55e96b3d1d6185b4e502a29286d19
ZiBuYuCHN <1063614727@qq.com>
2019-04-02 15:56:04 +0800

    测试换行

--- coolq/kjBot/modules/Asin/Forum/Threadcreate.php
+++ coolq/kjBot/modules/Asin/Forum/Threadcreate.php
@@ -24,7 +24,7 @@ class Threadcreate extends Module
         //     'doctype' => 2,
         //     'message' => implode("\n",explode('\n',$args['2']))
         // );
-        $msgData = "ajax=1&fromBot=1&fid=1&subject=".$args[1]."&doctype=2&message=".implode("\n",explode('\n',$args['2']));
+        $msgData = "ajax=1&fromBot=1&fid=1&subject=".$args[1]."&doctype=2&message=".implode("\r",explode('\n',$args['2']));
         Log::Debug('===<'.$msgData);
         $res = request_post('https://567.pohun.com/?thread-create.htm',$msgData);
         $res = json_decode($res,true);

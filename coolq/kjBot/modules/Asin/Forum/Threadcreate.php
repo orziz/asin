2bb4709f60bf1beed81a0919d41b97b7a5d1751a
ZiBuYuCHN <1063614727@qq.com>
2019-04-02 15:53:38 +0800

    修复bug

--- coolq/kjBot/modules/Asin/Forum/Threadcreate.php
+++ coolq/kjBot/modules/Asin/Forum/Threadcreate.php
@@ -16,15 +16,17 @@ class Threadcreate extends Module
 	public function process(array $args, $event){
         checkAuth($event,'master');
         // ajax=1&fromBot=1&fid=1&subject=test&doctype=2&message=test
-        $msgData = array(
-            'ajax' => 1,
-            'fromBot' => 1,
-            'fid' => 1,
-            'subject' => $args[1],
-            'doctype' => 2,
-            'message' => implode("\n",explode('\n',$args['2']))
-        );
-        $res = request_post('https://567.pohun.com/?thread-create.htm',json_encode($msgData));
+        // $msgData = array(
+        //     'ajax' => 1,
+        //     'fromBot' => 1,
+        //     'fid' => 1,
+        //     'subject' => $args[1],
+        //     'doctype' => 2,
+        //     'message' => implode("\n",explode('\n',$args['2']))
+        // );
+        $msgData = "ajax=1&fromBot=1&fid=1&subject=".$args[1]."&doctype=2&message=".implode("\n",explode('\n',$args['2']));
+        Log::Debug('===<'.$msgData);
+        $res = request_post('https://567.pohun.com/?thread-create.htm',$msgData);
         $res = json_decode($res,true);
         return $event->sendBack($res['message']);
     }

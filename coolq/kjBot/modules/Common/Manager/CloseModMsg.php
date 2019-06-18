0e90c768f86fc18fccf4fbbfd4598bc1f5d4a97c
ZiBuYuCHN <1063614727@qq.com>
2019-06-18 13:35:04 +0800

    123

--- coolq/kjBot/modules/Common/Manager/CloseModMsg.php
+++ coolq/kjBot/modules/Common/Manager/CloseModMsg.php
@@ -14,6 +14,8 @@ use \Log;
 class CloseModMsg extends Module
 {
 	public function process(array $args, $event){
-		return $event->sendBack('该功能已关闭');
+		$msg = '该功能已关闭';
+		$msg .= "\n现寻求人员参与某东西的开发测试，如果你会html、js、php、mysql或者绘图（ps或其他绘图软件均可）并对此感兴趣，请联系【子不语】";
+		return $event->sendBack($msg);
 	}
 }

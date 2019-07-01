fcbba447c00945fc8c019768dab66bc6d9248fb4
ZiBuYuCHN <1063614727@qq.com>
2019-07-01 15:00:55 +0800

    123

--- coolq/kjBot/modules/Common/Life/Trash.php
+++ coolq/kjBot/modules/Common/Life/Trash.php
@@ -25,18 +25,14 @@ class Trash extends Module
         $kw = urlencode($args[1]);
         $a = file_get_contents("http://trash.lhsr.cn/sites/feiguan/trashTypes_2/TrashQuery.aspx?kw={$kw}");
 
-        Log::Debug($a);
 
-        $cache1 = explode('<span style="font-size:.6rem; color:#2A2925;">', $a);
-
-        Log::Debug('-->'.count($cache1));
-        // if (count($cache1) < 2) {
-        //     $msg .= '你是什么垃圾？';
-        // } else {
-            $cache2 = explode('</span>', $cache1[1]);
-            Log::Debug('==>'.count($cache2));
-            $msg .= $cache2[0];
-        // }
+        $cache1 = explode('是指：', $a);
+        if (count($cache1) < 2) {
+            $msg .= '你是什么垃圾？';
+        } else {
+            $cache2 = explode('<b>', $cache1[0]);
+            $msg .= $cache2[count($cache2)-1];
+        }
         return $event->sendBack($msg);
         // echo $cache2[0];
     }

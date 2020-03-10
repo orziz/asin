dd5adee5b90a2c404d51ff70796a89aaaee7bca8
ZiBuYuCHN <1063614727@qq.com>
2020-03-10 11:38:53 +0800

    add

--- coolq/kjBot/plugins.php
+++ coolq/kjBot/plugins.php
@@ -4,5 +4,6 @@ $Plugins = [
     kjBotPlugin\GroupCheck\Common\_Message::class,
     kjBotPlugin\GroupCheck\AsinGroup\_Message::class,
     kjBotPlugin\GroupCheck\AsinGroup\_Notice::class,
-    kjBotPlugin\GroupCheck\AsinGroup\_Meta_event::class
+    kjBotPlugin\GroupCheck\AsinGroup\_Meta_event::class,
+    kjBotPlugin\GroupCheck\TRPG\_Message::class
 ];

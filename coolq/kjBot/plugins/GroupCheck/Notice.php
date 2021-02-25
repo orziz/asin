a7526645c0b21b50a8aff7127c24f2cf647bb224
ZiBuYuCHN <1063614727@qq.com>
2021-02-25 16:30:53 +0800

    debug

--- coolq/kjBot/plugins/GroupCheck/Notice.php
+++ coolq/kjBot/plugins/GroupCheck/Notice.php
@@ -15,8 +15,7 @@ class Notice extends Plugin {
 	public $handleQueue = true; //声明是否要捕获消息队列
 
 	// const cq_notice_group_increase = true;
-
-	public function beforePostMessage(&$queue){} //若声明不需要捕获消息队列可不实现本方法
+    
 	//此处以正常群聊消息举例
 	public function coolq_notice_friend_add($event,$cq): ?Message{
         PHFLog::Debug("debug---->$event");
